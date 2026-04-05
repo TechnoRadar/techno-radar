ca46f3ae4aca4b9ece72d010d52c5c9dda75695c
Luucaaass <lucas.rodher@gmail.com>
2026-04-05 12:33:05 +0100

    Fix: downgrade Java version to 21 to meet project requirements and finished sprint 1

--- github-feeder/src/main/java/es/ulpgc/dacd/control/GitHubClient.java
+++ github-feeder/src/main/java/es/ulpgc/dacd/control/GitHubClient.java
@@ -31,7 +31,6 @@ public class GitHubClient implements GitHubFeeder{
     @Override
     public List<GitHubTrend> getTrends() {
         List<GitHubTrend> trends = new ArrayList<>();
-        // URL de la API de GitHub (Ej: los repos de Java con más estrellas)
         String url = "https://api.github.com/search/repositories?q=stars:>50000&sort=stars&per_page=10";
 
         try {
